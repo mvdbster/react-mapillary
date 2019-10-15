2eb0156dee3b3db7aaba0d62ea32785389fca366
Alexander Lelidis <alelidis@esri.com>
2019-10-15 16:55:17 +0200

    make bearing event faster and add different handlers

--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,86 @@ import PropTypes from 'prop-types';
 import * as Mapillary from 'mapillary-js';
 
 class MapillaryViewer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tilt: 90,
+      fov: 55,
+    };
+  }
+
   componentDidMount() {
     this.viewer = new Mapillary.Viewer(
-      // Improvement pass a native element
       'mapillary-viewer',
       this.props.clientId,
+      null,
+      this.props.options,
     );
     // Initialize with .moveToKey because passing the imageKey in the constructor
     // does not update the viewer until the user clicks explore
+    this.viewer.on(Mapillary.Viewer.nodechanged, (event) => {
+      if (this.props.onNodeChanged) {
+        this.props.onNodeChanged(event);
+      }
+    });
+
+    this.viewer.on(Mapillary.Viewer.bearingchanged, (event) => {
+      if (this.props.onBearingChanged) {
+        this.props.onBearingChanged(event);
+      }
+    });
+
+    /* eslint-disable no-underscore-dangle */
+    this.viewer._container.renderService.renderCamera$.subscribe(
+      this.handleCameraChanges.bind(this),
+    );
+
+    this.viewer._container.renderService.bearing$.subscribe(
+      this.handleBearingChanges.bind(this),
+    );
+
+    this.handleWindowResize = this.handleWindowResize.bind(this);
+    window.addEventListener('resize', this.handleWindowResize);
+
     this.viewer.moveToKey(this.props.imageKey);
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.imageKey !== nextProps.imageKey) {
       this.viewer.moveToKey(nextProps.imageKey);
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize);
+  }
+
+  handleWindowResize() {
+    this.viewer.resize();
+  }
+
+  handleBearingChanges(bearing) {
+    if (this.props.onBearingChanged) {
+      this.props.onBearingChanged(bearing);
+    }
+  }
+
+  handleCameraChanges(camera) {
+    // subscripte to tilt changes
+    const tilt = (camera.rotation.theta * 180) / Math.PI;
+    if (tilt !== this.state.tilt) {
+      if (this.props.onTiltChanged) {
+        this.props.onTiltChanged(tilt);
+      }
+      this.setState({ tilt });
+    }
+
+    const fov = camera.perspective.fov;
+    if (fov !== this.state.fov) {
+      if (this.props.onFovChanged) {
+        this.props.onFovChanged(fov);
+      }
+      this.setState({ fov });
     }
   }
 
@@ -33,10 +99,20 @@ class MapillaryViewer extends Component {
 MapillaryViewer.propTypes = {
   clientId: PropTypes.string.isRequired,
   imageKey: PropTypes.string,
+  onNodeChanged: PropTypes.func,
+  onBearingChanged: PropTypes.func,
+  onTiltChanged: PropTypes.func,
+  onFovChanged: PropTypes.func,
+  options: PropTypes.object,
 };
 
 MapillaryViewer.defaultProps = {
   imageKey: '',
+  onNodeChanged: () => { },
+  onBearingChanged: () => { },
+  onTiltChanged: () => { },
+  onFovChanged: () => { },
+  options: null,
 };
 
 export {
